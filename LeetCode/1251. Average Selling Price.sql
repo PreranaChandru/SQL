5dda91391b95bc0fa6b08fbe8cf90586f0a32e4e
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2020-08-17 16:52:29 -0700

    Add files via upload

---
+++ LeetCode/1251. Average Selling Price.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id, ROUND(SUM(p.price * u.units)/SUM(u.units), 2) AS average_price
+FROM Prices p
+INNER JOIN UnitsSold u
+USING (product_id)
+WHERE  u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
