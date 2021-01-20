fe7b9cfeeb2f67fccc33f479465cb99ac4ab6a7c
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-01-19 16:06:38 -0800

    Update 1113. Reported Posts.sql

--- LeetCode/1113. Reported Posts.sql
+++ LeetCode/1113. Reported Posts.sql
@@ -1,4 +1,14 @@
+#SOLUTION 1:
+
 SELECT extra AS report_reason, COUNT(DISTINCT post_id) as report_count
 FROM Actions
 WHERE action = 'report' AND action_date = '2019-07-04'
 GROUP BY extra;
+
+
+#SOLUTION 2:
+
+SELECT extra AS 'report_reason', COUNT(DISTINCT post_id) AS 'report_count'
+FROM Actions
+WHERE action = 'report'AND DATEDIFF('2019-07-05', action_date) = 1
+GROUP BY extra ;
