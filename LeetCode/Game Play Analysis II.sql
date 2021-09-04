695eeddde87f0c6ea039d0be86a9f3f4db3f936c
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-09-04 14:50:41 -0700

    Update Game Play Analysis II.sql

--- LeetCode/Game Play Analysis II.sql
+++ LeetCode/Game Play Analysis II.sql
@@ -1,3 +1,5 @@
+# SOLUTION 1 (Using Subquery)
+
 SELECT player_id, device_id
 FROM Activity
 WHERE (player_id, event_date) IN 
@@ -5,3 +7,9 @@ WHERE (player_id, event_date) IN
     FROM Activity
     GROUP BY player_id)
 ;
+
+#SOLUTION 2 (Using Window Function)
+
+SELECT DISTINCT player_id, 
+       FIRST_VALUE(device_id) OVER(PARTITION BY player_id ORDER BY event_date) AS device_id
+FROM ACtivity;
