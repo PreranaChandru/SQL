7f40fef40a2bb3785bd05b1bf9f0b3be24c4105f
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-09-04 14:52:29 -0700

    Update Game Play Analysis I.sql

--- LeetCode/Game Play Analysis I.sql
+++ LeetCode/Game Play Analysis I.sql
@@ -1,3 +1,12 @@
+# SOLUTION 1
+
 SELECT player_id, MIN(event_date) as first_login 
 FROM Activity
 GROUP BY player_id;
+
+# SOLUTION 2 (Using Window Function)
+
+SELECT DISTINCT player_id, 
+       FIRST_VALUE(event_date) OVER(PARTITION BY player_id ORDER BY event_date) AS first_login
+FROM Activity;
+
