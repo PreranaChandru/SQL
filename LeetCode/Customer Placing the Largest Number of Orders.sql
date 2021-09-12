e46ab139eb798da6c0392601cdadaf350754ac0f
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-09-11 17:43:17 -0700

    Update Customer Placing the Largest Number of Orders.sql

--- LeetCode/Customer Placing the Largest Number of Orders.sql
+++ LeetCode/Customer Placing the Largest Number of Orders.sql
@@ -3,3 +3,16 @@ FROM orders
 GROUP BY customer_number
 ORDER BY COUNT(*) DESC
 LIMIT 1;
+
+
+# SOLUTION 2 (Using Window Function)
+
+SELECT customer_number 
+FROM 
+    (
+        SELECT customer_number,
+               DENSE_RANK() OVER(ORDER BY COUNT(order_number) DESC) AS dr
+        FROM Orders
+        GROUP BY customer_number
+    ) AS temp
+WHERE temp.dr = 1
