803de9cac30d9af89fa4dd07875b672c385dec79
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-09-11 19:07:29 -0700

    Update 574. Winning Candidate.sql

--- LeetCode/574. Winning Candidate.sql
+++ LeetCode/574. Winning Candidate.sql
@@ -5,3 +5,28 @@ ON c.id = v.candidateId
 GROUP BY CandidateId
 ORDER BY COUNT(CandidateId) DESC
 LIMIT 1
+
+
+
+SELECT name 
+FROM Candidate
+WHERE Id =
+        (SELECT CandidateId
+        FROM Vote
+        GROUP BY CandidateId
+        ORDER BY COUNT(*) DESC
+        LIMIT 1)
+        
+        
+SELECT Name 
+FROM Candidate c
+INNER JOIN  
+        (SELECT CandidateId,
+                DENSE_RANK() OVER(ORDER BY COUNT(CandidateId) DESC) AS dr
+         FROM Vote       
+         GROUP BY CandidateId
+        ) AS temp
+ON c.id = temp.candidateId
+WHERE temp.dr = 1
+
+        
