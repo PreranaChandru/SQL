48243c7140a87ea31c6c550890388e355b1252f4
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-09-11 18:08:37 -0700

    Update Rising Temperature.sql

--- LeetCode/Rising Temperature.sql
+++ LeetCode/Rising Temperature.sql
@@ -3,3 +3,15 @@ FROM Weather w1
 JOIN Weather w2
 ON DATEDIFF(w2.RecordDate, w1.RecordDate) = 1 
 WHERE w2.Temperature > w1.Temperature;
+
+# SOLUTION 2 (Using Window Function)
+
+SELECT id
+FROM 
+(
+    SELECT id,
+           Temperature - LAG(Temperature) OVER(ORDER BY recordDate) AS temp_diff,
+           DATEDIFF(recordDate, LAG(recordDate) OVER(ORDER BY recordDate)) AS date_diff
+    FROM Weather 
+) AS t
+WHERE t.temp_diff > 0  AND t.date_diff = 1
