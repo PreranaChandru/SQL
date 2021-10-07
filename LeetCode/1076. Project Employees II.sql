93173af2f5fbb366f5ca326514fb6d8682774ea7
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-10-06 23:27:28 -0700

    Update 1076. Project Employees II.sql

--- LeetCode/1076. Project Employees II.sql
+++ LeetCode/1076. Project Employees II.sql
@@ -6,3 +6,16 @@ HAVING COUNT(*) = (SELECT COUNT(*)
                     GROUP BY project_id
                     ORDER BY COUNT(*) DESC
                     LIMIT 1);
+                    
+                    
+                    
+# SOLUTION 2
+
+WITH CTE AS
+(SELECT project_id, DENSE_RANK() OVER(ORDER BY COUNT(employee_id) DESC) AS dr
+FROM Project
+GROUP BY project_id)
+
+SELECT project_id
+FROM CTE
+WHERE dr = 1
