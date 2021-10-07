42a390f307750ea63f40885b849c41048dc561ce
Prerana Chandrashekar <prerana.chandru95@gmail.com>
2021-10-06 23:01:24 -0700

    Update 1075. Project Employees I.sql

--- LeetCode/1075. Project Employees I.sql
+++ LeetCode/1075. Project Employees I.sql
@@ -3,3 +3,11 @@ FROM Project p
 INNER JOIN Employee e
 USING (employee_id)
 GROUP BY p.project_id;
+
+
+
+SELECT DISTINCT p.project_id, 
+       ROUND((AVG(e.experience_years) OVER(PARTITION BY p.project_id)),2) AS average_years
+FROM Project p
+JOIN Employee e
+    USING(employee_id)
